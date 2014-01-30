eda993540d82806673f4b60d3042e7908a5cb75a
Jaime Williams <jaime.williams@eagles.ewu.edu>
2014-01-29 16:25:35 -0800

    changed super secret code

--- management.php
+++ management.php
@@ -2,7 +2,7 @@
 	define( "DB_HOST", "localhost" );
 	define( "DB_NAME", "devfoudo_webapp" );
 	define( "DB_USER", "devfoudo_jaime" );
-	define( "DB_PASS", "hk@7g.QUKn40" );
+	define( "DB_PASS", "" );
 
 	require('Database.class.php');
 
